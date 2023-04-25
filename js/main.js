86d72e8af52447bf4ba0ac85a45ee8c11037798c
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-04-25 14:46:10 -0300

    atualizando

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+const controle = document.querySelectorAll("[data-controle]")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        
+    })
+}) 
+
+  function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]")
+
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1       
+  } else {
+    peca.value = parseInt(peca.value) +1
+  }
+}
