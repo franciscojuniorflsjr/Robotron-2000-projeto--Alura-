ee7ed91eeba5a733fd37afafbca8139fc64af92e
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-04-26 13:59:52 -0300

    Concluído projeto  Robotron 2000

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+console.log(estatisticas)
 const pecas = {
   "bracos": {
       "forca": 29,
@@ -36,7 +38,7 @@ const pecas = {
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
-        
+        atualizaEstatisticas(evento.target.dataset.peca)
     })
 }) 
 
@@ -48,4 +50,13 @@ controle.forEach( (elemento) => {
   } else {
     peca.value = parseInt(peca.value) +1
   }
+}
+
+function atualizaEstatisticas (peca) {
+
+
+estatisticas.forEach( (elemento) => {
+   elemento.textContent = parseInt(elemento.textContent) + pecas [peca][elemento.dataset.estatistica]
+
+})
 }
